f9eeb31b7c9ffce72d653dc8a230db1cfe7efd83
Pranay <pranay.pratap15@gmail.com>
2025-06-07 04:05:05 +0530

    Changes to be committed: 	modified:   frontend/src/components/Avatar.tsx

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,8 +1,8 @@
 const Avatar = ({name}: {name: string})=> {
     const firstLetter = name?.charAt(0).toUpperCase()
     return (
-        <div className="relative inline-flex items-center justify-center w-5 h-5 overflow-hidden bg-white rounded-full dark:bg-gray-500">
-            <span className="text-xs text-white dark:text-gray-100">{firstLetter}</span>
+        <div className="relative inline-flex items-center justify-center w-5 h-5 overflow-hidden bg-gray-200 rounded-full dark:bg-gray-500">
+            <span className="text-xs text-black dark:text-gray-100">{firstLetter}</span>
         </div>
     )
 }
